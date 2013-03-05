b803ddb6d4382325bad4c152b94008e6df294ef9
Richard Fuchshuber <richardfuch@gmail.com>
2013-03-05 19:41:13 -0300

    refatoracao da CardView

--- YAPPA/CardView.m
+++ YAPPA/CardView.m
@@ -1,6 +1,7 @@
 #import "CardView.h"
 
 #define CARD_CORNER_RADIUS 10.0
+#define FONT_RATIO 0.5
 
 @implementation CardView
 
@@ -16,15 +17,54 @@
 - (void)drawRect:(CGRect)rect
 {
     // Drawing code
+    [self drawCard];
+    
+//    [[UIColor blackColor] setStroke];
+//    [roundedRect stroke];
+    
+    [self drawCardText: self.cardText];
+    
+}
+
+- (void) drawCard {
     UIBezierPath *roundedRect = [UIBezierPath bezierPathWithRoundedRect:self.bounds cornerRadius:CARD_CORNER_RADIUS];
     
     [roundedRect addClip];
     
     [[UIColor whiteColor] setFill];
     UIRectFill(self.bounds);
+}
+
+- (void) drawCardText:(NSString *) text {
+    CGPoint middle = [self calculateMiddlePointOfCard];
+    UIFont *font = [self generateFontWithCorrectSize];
     
-    [[UIColor blackColor] setStroke];
-    [roundedRect stroke];
+    NSAttributedString *attributedText = [self generateCardContentWithFont: font];
+
+    CGSize textSize = [attributedText size];
+    CGPoint textOrigin = [self calculateTextOrigin:middle withTextSize:textSize];
+
+    [attributedText drawAtPoint:textOrigin];
+}
+
+- (CGPoint) calculateMiddlePointOfCard {
+    return CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2);
+}
+
+- (UIFont *) generateFontWithCorrectSize {
+    return [UIFont systemFontOfSize:self.bounds.size.width * FONT_RATIO];
+}
+
+- (CGPoint) calculateTextOrigin: (CGPoint) middleOfCard
+                   withTextSize: (CGSize) textSize {
+    
+    return CGPointMake(middleOfCard.x - textSize.width/2.0,
+                       middleOfCard.y - textSize.height/2.0);
+    
+}
+
+- (NSAttributedString *) generateCardContentWithFont: (UIFont *) font {
+    return [[NSAttributedString alloc] initWithString:self.cardText attributes:@{NSFontAttributeName : font}];
 }
 
 - (void) setFaceUp:(BOOL)faceUp {
