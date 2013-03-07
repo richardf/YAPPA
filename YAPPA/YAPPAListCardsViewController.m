620f8e1fa8c11b03c3714c1f189959b06d10d31d
Richard Fuchshuber <richardfuch@gmail.com>
2013-03-06 21:00:52 -0300

    criacao do controller da lista de cartas

---
+++ YAPPA/YAPPAListCardsViewController.m
@@ -0,0 +1,38 @@
+//
+//  YAPPAListCardsViewController.m
+//  YAPPA
+//
+//  Created by Richard Fuchshuber on 05/03/13.
+//  Copyright (c) 2013 Richard Fuchshuber. All rights reserved.
+//
+
+#import "YAPPAListCardsViewController.h"
+
+@interface YAPPAListCardsViewController ()
+
+@end
+
+@implementation YAPPAListCardsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
